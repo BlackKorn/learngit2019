090019ddcb02e280e4d82ef313b681513644f247
Korn <xukan10@126.com>
2019-06-12 23:02:46 +0800

    First Commit

---
+++ WebDemo/src/com/korn/test/Test.java
@@ -0,0 +1,57 @@
+package com.korn.test;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class Test {
+	/**
+     * post请求，参数为json字符串
+     * @param url 请求地址
+     * @param jsonString json字符串
+     * @return 响应
+     */
+    public String postJson(String url,String jsonString)
+    {
+        String result = null;
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost post = new HttpPost(url);
+        CloseableHttpResponse response = null;
+        try {
+            post.setEntity(new ByteArrayEntity(jsonString.getBytes("UTF-8")));
+            response = httpClient.execute(post);
+            if(response != null && response.getStatusLine().getStatusCode() == 200)
+            {
+                HttpEntity entity = response.getEntity();
+                result = entity.toString();
+            }
+            return result;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                httpClient.close();
+                if(response != null)
+                {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+}
